51943bf504b3ee96c289ffaa197ed6f1e7dd6a2d
smalldazzy <oleg.nod@gmail.com>
2019-08-09 23:01:09 +0300

    prettyfied

--- src/webparts/search/components/Search.tsx
+++ src/webparts/search/components/Search.tsx
@@ -7,9 +7,8 @@ import pnp, { Web, sp } from 'sp-pnp-js';
 
 export default class Search extends React.Component<ISearchProps, {}> {
   public state = {
-    rawitems: [],
     columns: [],
-    refitems:[]
+    refitems: []
   };
   public render(): React.ReactElement<ISearchProps> {
     return (
@@ -17,8 +16,6 @@ export default class Search extends React.Component<ISearchProps, {}> {
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint!</span>
-              <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
               <DetailsList
                 items={this.state.refitems}
                 compact={false}
@@ -32,9 +29,6 @@ export default class Search extends React.Component<ISearchProps, {}> {
                 ariaLabelForSelectAllCheckbox="Toggle selection for all items"
               />
               <p className={styles.description}>{escape(this.props.listname)}</p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Learn more</span>
-              </a>
             </div>
           </div>
         </div>
@@ -42,12 +36,9 @@ export default class Search extends React.Component<ISearchProps, {}> {
     );
   }
   public componentDidMount() {
-    // this.getData();
     this.setState({ columns: this.columsCreate(['Id', 'Title', 'Color']) });
     this.searchList();
 
-  }
-  public componentWillReceiveProps(){
   }
   private columsCreate(arraySelect: Array<any>): Array<IColumn> {
     const columns: IColumn[] = [];
@@ -65,45 +56,25 @@ export default class Search extends React.Component<ISearchProps, {}> {
   }
   private searchList() {
     let refitems = [];
-    //  https://cupcuper.sharepoint.com/search/_api/search/query?querytext='colors'&clienttype='ContentSearchRegular'
-    console.log(this.props.listname);
     fetch(`https://cupcuper.sharepoint.com/search/_api/search/query?querytext='b40ba88e-c159-47ad-bef0-43ea539cf24c'&selectproperties='title%2cRefinableString50'&clienttype='ContentSearchRegular'`, {
       method: 'get',
       headers: {
         'accept': "application/json;odata=verbose",
-        'content-type': "application/json;odata=verbose", 
+        'content-type': "application/json;odata=verbose",
       }
-    }).then((response)=>response.json()).then((d)=>{
-      console.log(d.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results);
+    }).then((response) => response.json()).then((d) => {
       return d.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
     }
-    ).then((data)=> {
+    ).then((data) => {
       console.log(data);
       data.forEach(item => {
-        // console.log(item.Cells.results[2].Value)
         refitems.push({
           Id: item.Cells.results[1].Value,
           Title: item.Cells.results[2].Value,
           Color: item.Cells.results[3].Value
-        }),this.setState({refitems: refitems});
-        
+        }), this.setState({ refitems: refitems });
+
       });
-    }).then(()=>console.log(refitems));
-  }
-  private getData(): void {
-    // let uri = this.props.siteurl || 'sites/dev1';
-    // console.log(this.props.siteurl);
-    let guid = /*this.props.listdropdown ||*/ '78d16b51-cb43-4802-a42b-3713059cd18b';
-    let columns = this.columsCreate(['Id', 'Title']);
-    this.setState({ columns: columns });
-    console.log(this.props.context.pageContext.web);
-    let wep = new Web(this.props.context.pageContext.web.absoluteUrl + '/sites/dev1');
-    // pnp.sp.web.lists.filter('Hidden eq false').get().then((li) => console.log(li));
-    wep.lists.getById(guid).items.get().then
-      ((response) => {
-        this.setState({ rawitems: response });
-        console.log('writing');
-      }
-      ).then(() => console.log(this.state));
+    });
   }
 }
